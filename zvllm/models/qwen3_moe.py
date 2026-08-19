Fix MoE expert routing weight gather (broadcast shape bug)

zvllm/models/qwen3_moe.py
@@ -87,7 +87,8 @@ class Qwen3MoeSparseMoeBlock(nn.Module):
                 continue
             idx = mask.nonzero()
             y = expert(x[idx[:, 0]])
-            out[idx[:, 0], idx[:, 1]] = y * topk_weights[idx]
+            w = topk_weights[idx[:, 0], idx[:, 1]].unsqueeze(-1)
+            out[idx[:, 0], idx[:, 1]] = y * w
         if self.tp_size > 1:
             dist.all_reduce(out, group=self.tp_group)
         if self.ep_size > 1: